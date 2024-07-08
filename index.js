6b8f9149ad08bd6e682f5e4c7f2639514497959f
eustachekamala <eustachejared25@gmail.com>
2024-07-08 23:15:59 +0300

    I did the function writeCards with there parametres(name, event) this method return an array of a message plus the arguments of the array names[] plus the event and I created a function of countDown(starting) this method invokes console.log once for each number

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+
+
+const writeCards = (name, event) => {
+    const array = []
+    for (let i = 0; i < name.length; i++) {
+        array.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`);
+    }
+    return array;
+}
+
+
+
+const countDown = (starting) => {
+    for (let index = starting; index >= 0; index--) {
+        console.log(index);
+    }
+}
+
+writeCards(["Guadalupe", "Ollie", "Aki"], "surprise");
+countDown(11);
